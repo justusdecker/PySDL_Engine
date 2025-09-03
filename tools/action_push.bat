9ae36ef57925618d4b0a40c0e5330fb07e3a4b1e
Justus <justus.d2025@gmail.com>
2025-09-03 13:23:30 +0200

    0.1.79:bcc83cb - remove cm.i

--- tools/action_push.bat
+++ tools/action_push.bat
@@ -1,9 +1,10 @@
 WScript.exe ./tools/get_current_commit_msg.vbs
-git add -A :!./tools/cm.i
+
 git log --pretty=format:%%h -n 1 > ./tools/hash.i
 python ./tools/easy_commit.py
 set /p ver=<./tools/cmtmsg.i
 set /p hsh=<./tools/hash.i
 set /p msg=<./tools/cm.i
+git add -A :!./tools/cm.i :!./tools/hash.i :!./tools/cmtmsg.i
 git commit -m "%ver%:%hsh% - %msg%"
 git push

f09a3abc3a45d86ef7165140b0db2c8eabef446c
Justus <justus.d2025@gmail.com>
2025-09-03 13:06:41 +0200

    0.1.70:6eb6d9a - fixed easy_commit message

--- tools/action_push.bat
+++ tools/action_push.bat
@@ -2,9 +2,8 @@ git add -A
 WScript.exe ./tools/get_current_commit_msg.vbs
 git log --pretty=format:%%h -n 1 > ./tools/hash.i
 python ./tools/easy_commit.py
-set /p msg=<./tools/cmtmsg.i
+set /p ver=<./tools/cmtmsg.i
 set /p hsh=<./tools/hash.i
-
-
-git commit -m "%msg%%hsh%"
+set /p msg=<./tools/cm.i
+git commit -m "%ver%:%hsh% - %msg%"
 git push
